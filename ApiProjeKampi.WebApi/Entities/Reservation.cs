0ae2cbb919b15d5a8b958f7adb5546b8fb25ce7d
Ali Altunoğlu <alialtunoglu02@gmail.com>
2025-02-05 23:21:46 +0300

    add entities

--- ApiProjeKampi.WebApi/Entities/Reservation.cs
+++ ApiProjeKampi.WebApi/Entities/Reservation.cs
@@ -7,7 +7,7 @@ namespace ApiProjeKampi.WebApi.Entities
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public DateTime ReservationDate { get; set; }
-        public TimeOnly ReservationTime { get; set; }
+        public TimeSpan ReservationTime { get; set; }
         public int CountOfPeople { get; set; }
         public string Message { get; set; }
         public string ReservationStatus { get; set; }
